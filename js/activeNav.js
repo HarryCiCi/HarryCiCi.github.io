384c44e044e92060b52a9ee4160ec294fdf8c8cd
HarryCiCi <1051062188@qq.com>
2020-08-19 00:40:12 +0800

    Site updated: 2020-08-19 00:40:01

--- js/activeNav.js
+++ js/activeNav.js
@@ -1,9 +1,17 @@
 // which nav has active
 let navs = document.querySelectorAll('.nav-item');
+let bgs = document.querySelector('#bgs');
 let pagePath = window.location.pathname;
 for(let nav of navs) {
   let navPath = nav.getAttribute("data-path");
   if(navPath && navPath === pagePath) {
     nav.className = "nav-item active";
   }
+  if(nav.key=='music'){
+    bgs.width='100%'.backgroundImage='url(theme.avatar.bgp)';
+  }
+  if(nav.key=='about'){
+    bgs.width='100%'.backgroundImage='url(/images/pabout.jpg)';
+  }
 }
+
