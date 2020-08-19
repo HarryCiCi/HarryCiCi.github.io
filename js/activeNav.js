40e273f545c0154b4c7a38b9fe810c3a239b1148
HarryCiCi <1051062188@qq.com>
2020-08-19 11:11:27 +0800

    Site updated: 2020-08-19 11:11:11

--- js/activeNav.js
+++ js/activeNav.js
@@ -1,22 +1,20 @@
 // which nav has active
 let navs = document.querySelectorAll('.nav-item');
 let bgs = document.getElementById('bgs');
+bgs.className=bgs.className+' '+'bgm';
 let pagePath = window.location.pathname;
 for(let nav of navs) {
   let navPath = nav.getAttribute("data-path");
   if(navPath && navPath === pagePath) {
     nav.className = "nav-item active";
-    body.style.color = '#39f';
   }
   if(nav.getAttribute('key')==='music'){
    
-    body.style.color = '#39f';
-    bgs.addClass('bgm');
   }
   if(nav.getAttribute('key')==='about'){
     bgs.style.color='#0f0'
     bgs.addClass('bga');
-    bgs.className=bgs.className+' '+'bgm';
+    
   }
 }
 
